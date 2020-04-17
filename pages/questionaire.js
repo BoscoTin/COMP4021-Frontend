4173af59dfc98153bf3dc96466e1d0c2baf9629f
Chi Hang Dai <chihangdai@chide-mbp.lan>
2020-04-17 20:32:27 +0800

    change some duplicate and minor mistake

--- pages/questionaire.js
+++ pages/questionaire.js
@@ -11,7 +11,6 @@ import Box from "@material-ui/core/Box";
 import Link from "../src/components/Link";
 import Radio from "@material-ui/core/Radio";
 import FormControl from "@material-ui/core/FormControl";
-import FormLabel from "@material-ui/core/FormLabel";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
@@ -23,64 +22,65 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 */
 const useStyles = makeStyles((theme) => ({
   Background:{
-    backgroundColor: theme.palette.text.primary,
-    position: "relative",
-    width: "1600px",
-    height: "1024px"
+      backgroundColor: theme.palette.text.primary,
+      border: "1px solid #979797",
+      position: "relative",
+      width: "1600px",
+      height: "1024px"
   },
 
   rightLayer:{
-    position: "absolute",
-    left: "11.38%",
-    right: "11.44%",
-    top: "15.14%",
-    bottom: "15.14%",
-    background: theme.palette.text.secondary,
-    borderRadius:"12px",
-    width:"1235px"
+      position: "absolute",
+      left: "11.38%",
+      right: "11.44%",
+      top: "15.14%",
+      bottom: "15.14%",
+      background: theme.palette.text.secondary,
+      borderRadius:"12px",
+      width:"1235px"
   }, rightLayerContent1:{
-    position: "absolute",
-    width: "587px",
-    height: "100px",
-    left: "480px",
-    top: "120px"
+        position: "absolute",
+        width: "587px",
+        height: "100px",
+        left: "480px",
+        top: "120px"
   }, rightLayerButton1:{
-    position: "absolute",
-    width: "62px",
-    height: "16px",
-    left: "856px",
-    top: "570px",
-    background:theme.palette.text.secondary
+        position: "absolute",
+        width: "62px",
+        height: "16px",
+        left: "856px",
+        top: "570px",
+        background:theme.palette.text.secondary
   }, rightLayerButton2:{
-    position: "absolute",
-    width: "94px",
-    height: "36px",
-    left: "938px",
-    top: "560px",
-    background:theme.palette.primary.main
+        position: "absolute",
+        width: "94px",
+        height: "36px",
+        left: "938px",
+        top: "560px",
+        background:theme.palette.primary.main
   }, rightLayerButtonBoxContainer:{
-    position: "absolute",
-    width: "552px",
-    height: "288px",
-    left: "480px",
-    top: "230px",
-    background:"#FFFFFF",
-    boxShadow: "0px 2px 4px rgba(96, 97, 112, 0.16), 0px 0px 1px rgba(40, 41, 61, 0.04)",
-    borderRadius: "6px"
-  }, rightLayerButtonTitleContainer:{
-        width: "300px",
-        height: "248px",
-        left: "3px"
+        position: "absolute",
+        width: "552px",
+        height: "288px",
+        left: "480px",
+        top: "230px",
+        background:"#FFFFFF",
+        boxShadow: "0px 2px 4px rgba(96, 97, 112, 0.16), 0px 0px 1px rgba(40, 41, 61, 0.04)",
+        borderRadius: "6px"
+  },rightLayerButtonTitleContainer:{
+          width: "300px",
+          height: "248px",
+          left: "3px"
     },rightLayerButtonTitle:{
-      width: "140.55px",
-      height: "24px",
-      marginTop:"13px",
-      marginLeft:"16px",
-      marginBottom:"11px"
+          width: "140.55px",
+          height: "24px",
+          marginTop:"13px",
+          marginLeft:"16px",
+          marginBottom:"11px"
   }, rightLayerButton:{
-      top:"6.25%",
-      left:"93.75%",
-      icon:"#0099FF"
+          top:"6.25%",
+          left:"93.75%",
+          icon:"#0099FF"
   },
 
   leftLayer:{
@@ -121,20 +121,20 @@ const useStyles = makeStyles((theme) => ({
             outlineOffset: 2,
         },
         'input:hover ~ &': {
-            backgroundColor: '#ebf1f5',
+            backgroundColor: '#FFFFFF',
         },
         'input:disabled ~ &': {
             boxShadow: 'none',
             background: 'rgba(206,217,224,.5)',
         },
     }, CheckedIcon: {
-        backgroundColor: '#137cbd',
+        backgroundColor: '#FFFFFF',
         backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
         '&:before': {
             display: 'block',
             width: 14,
             height: 14,
-            backgroundImage: 'radial-gradient(#fff,#fff 28%,transparent 32%)',
+            backgroundImage: 'radial-gradient(#0099FF,#0099FF 28%,transparent 32%)',
             content: '""',
         },
         'input:hover ~ &': {
@@ -158,31 +158,39 @@ function LeftLayer(props){
       <Grid container className = {classes.leftLayer}>
 
           <Grid container className = {classes.leftLayerContext1}>
-            <Grid item >
-              <Typography variant="h4" color="#000000">
-                <Box fontFamily={"Asap"}
-                     fontStyle={"normal"}
-                     fontWeight={"normal"}
-                     fontSize={"36px"}
-                     lineHeight={"130%"}>
-                  Let Us Start <br />With Your<br />Personality Test
-                </Box>
-              </Typography>
-            </Grid>
+              <Grid item >
+                  <Typography variant="h4" color="#000000">
+                      <Box fontFamily={"Asap"}
+                           fontStyle={"normal"}
+                           fontWeight={"normal"}
+                           fontSize={"36px"}
+                           lineHeight={"130%"}>
+                          Let Us Start With Your Personality Test
+                      </Box>
+                  </Typography>
+              </Grid>
           </Grid>
 
           <Grid container className = {classes.leftLayerContext2}>
-            <Grid item >
-              <Typography color="textSecondary" >
-                <Box fontFamily={"Asap"}
-                     fontStyle={"normal"}
-                     fontWeight={"normal"}
-                     fontSize={"20px"}
-                     lineHeight={"130%"}>
-                  Tell us more about your <br/>value, your style and your personality
-                </Box>
-              </Typography>
-            </Grid>
+              <Grid item >
+                  <Typography color="textSecondary" >
+                      <Box fontFamily={"Asap"}
+                         fontStyle={"normal"}
+                         fontWeight={"normal"}
+                         fontSize={"20px"}
+                         lineHeight={"130%"}>
+                          Tell us more about your
+                      </Box>
+
+                      <Box fontFamily={"Asap"}
+                           fontStyle={"normal"}
+                           fontWeight={"normal"}
+                           fontSize={"20px"}
+                           lineHeight={"130%"}>
+                          value, your style and your personality
+                      </Box>
+                  </Typography>
+              </Grid>
           </Grid>
 
       </Grid>
@@ -194,39 +202,39 @@ function RightLayer(props){
   return (
       <Grid container className = {classes.rightLayer}>
 
-        <LeftLayer classes = {classes} />
+          <LeftLayer classes = {classes} />
 
-        <Grid container className={classes.rightLayerContent1}>
-          <Typography>
-            <Box fontFamily={"Roboto"}
-                 fontStyle={"normal"}
-                 fontWeight={"normal"}
-                 fontSize={"18px"}
-                 lineHeight={"40px"}
-                 color={"#D5DDE3"}>
-                1 of 6
-            </Box>
-            <Box fontFamily={"Roboto"}
-                 fontStyle={"normal"}
-                 fontWeight={"normal"}
-                 fontSize={"22px"}
-                 lineHeight={"40px"}
-                 color={"#000"}>
-                How would you describe yourself? Active or Passive
-            </Box>
-          </Typography>
+          <Grid container className={classes.rightLayerContent1}>
+              <Typography>
+                  <Box fontFamily={"Roboto"} fontStyle={"normal"}
+                     fontWeight={"normal"}
+                     fontSize={"18px"}
+                     lineHeight={"40px"}
+                     color={"#D5DDE3"}>
+                    1 of 6
+                  </Box>
 
-        </Grid>
-        <Button1 classes = {classes}/>
-        <Button2 classes = {classes}/>
-        <CheckBox classes = {classes}/>
+                  <Box fontFamily={"Roboto"}
+                     fontStyle={"normal"}
+                     fontWeight={"normal"}
+                     fontSize={"22px"}
+                     lineHeight={"40px"}
+                     color={"#000"}>
+                    How would you describe yourself? Active or Passive
+                  </Box>
+              </Typography>
+
+          </Grid>
+
+          <Button1 classes = {classes}/>
+          <Button2 classes = {classes}/>
+          <CheckBox classes = {classes}/>
       </Grid>
   );
 }
 
 function StyledRadio(props) {
     const classes = useStyles();
-
     return (
         <Radio
             className={classes.ButtonRoot}
@@ -241,72 +249,59 @@ function StyledRadio(props) {
 
 function CheckBox(props){
   const {classes} = props;
-    const [value, setValue] = React.useState("female");
+  const [value, setValue] = React.useState("female");
 
-    function handleClick(event) {
-        if (event.target.value === value) {
-            setValue("");
-        } else {
-            setValue(event.target.value);
-        }
-    }
+  function handleClick(event) {
+      if (event.target.value === value) {
+          setValue("");
+      } else {
+          setValue(event.target.value);
+      }
+  }
+
+  function RightLayerButtonTitleBox(props){
+      const classes = useStyles();
+      return(
+          <Box
+              className={classes.rightLayerButtonTitle}
+              fontFamily= "Roboto"
+              font-style={"normal"}
+              fontWeight={"normal"}
+              fontSize= "14px"
+              lineHeight= "24px"
+              {...props}/>
+              );
+  }
 
   return(
       <Grid container className={classes.rightLayerButtonBoxContainer} >
           <Box className={classes.rightLayerButtonTitleContainer} display={"flex"} flexDirection={"column"}>
-              <Box className={classes.rightLayerButtonTitle}
-                   fontFamily= "Roboto"
-                   font-style={"normal"}
-                   fontWeight={"normal"}
-                   fontSize= "14px"
-                   lineHeight= "24px">
+              <RightLayerButtonTitleBox>
                   Helsinki
-              </Box>
-              <Box className={classes.rightLayerButtonTitle}
-                   fontFamily= "Roboto"
-                   font-style={"normal"}
-                   fontWeight={"normal"}
-                   fontSize= "14px"
-                   lineHeight= "24px">
+              </RightLayerButtonTitleBox>
+
+              <RightLayerButtonTitleBox>
                   Helsinki
-              </Box>
-              <Box className={classes.rightLayerButtonTitle}
-                         fontFamily= "Roboto"
-                         font-style={"normal"}
-                         fontWeight={"normal"}
-                         fontSize= "14px"
-                         lineHeight= "24px">
-              Helsinki
-              </Box>
-              <Box className={classes.rightLayerButtonTitle}
-                     fontFamily= "Roboto"
-                     font-style={"normal"}
-                     fontWeight={"normal"}
-                     fontSize= "14px"
-                     lineHeight= "24px">
-              Helsinki
-              </Box>
-              <Box className={classes.rightLayerButtonTitle}
-                     fontFamily= "Roboto"
-                     font-style={"normal"}
-                     fontWeight={"normal"}
-                     fontSize= "14px"
-                     lineHeight= "24px">
-              Helsinki
-              </Box>
-              <Box className={classes.rightLayerButtonTitle}
-                   fontFamily= "Roboto"
-                   font-style={"normal"}
-                   fontWeight={"normal"}
-                   fontSize= "14px"
-                   lineHeight= "24px">
+              </RightLayerButtonTitleBox>
+
+              <RightLayerButtonTitleBox>
                   Helsinki
-              </Box>
+              </RightLayerButtonTitleBox>
+
+              <RightLayerButtonTitleBox>
+                  Helsinki
+              </RightLayerButtonTitleBox>
+
+              <RightLayerButtonTitleBox>
+                  Helsinki
+              </RightLayerButtonTitleBox>
+
+              <RightLayerButtonTitleBox>
+                  Helsinki
+              </RightLayerButtonTitleBox>
           </Box>
           <FormControl component="fieldset" className={classes.formControl}>
               <RadioGroup
-                  aria-label="gender"
-                  name="gender1"
                   className={classes.group}
                   value={value}>
                   <FormControlLabel
@@ -316,7 +311,6 @@ function CheckBox(props){
                   <FormControlLabel
                       value="male"
                       control={<StyledRadio onClick={handleClick} />}
-
                   />
                   <FormControlLabel
                       value="other"
@@ -325,7 +319,6 @@ function CheckBox(props){
                   <FormControlLabel
                       value="1"
                       control={<StyledRadio onClick={handleClick} />}
-
                   />
                   <FormControlLabel
                       value="2"
@@ -347,9 +340,8 @@ function Button1(props){
       <Button
           className={classes.rightLayerButton1}
           href="/signup"
-          component={Link}
-          variant="contained">
-        <Typography variant="button" color={"#979797"}>CANCEL</Typography>
+          component={Link}>
+        <Typography color={"#979797"}>CANCEL</Typography>
       </Button>
   );
 }
@@ -366,10 +358,7 @@ function Button2(props){
   );
 }
 
-
-
 export default function QuestionairePage() {
   const classes = useStyles();
-
   return <BackGround classes = {classes} />;
 }
