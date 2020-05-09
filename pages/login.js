3664a6e22a4a5c45930086778da34171bcc4ceb9
Chi Hang Dai <chihangdai@chide-mbp.lan>
2020-05-10 01:06:42 +0800

    finish the checkBox handling

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import {Grid, Box, Typography, Button} from "@material-ui/core";
 import Link from "../src/components/Link";
-import {CheckBox, Visibility, VisibilityOff} from "@material-ui/icons";
+import {Visibility, VisibilityOff} from "@material-ui/icons";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import IconButton from "@material-ui/core/IconButton";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
@@ -10,6 +10,8 @@ import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import FingerprintIcon from '@material-ui/icons/Fingerprint';
+import Checkbox from '@material-ui/core/Checkbox';
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
     Root: {
@@ -34,13 +36,48 @@ const useStyles = makeStyles((theme) => ({
         marginRight:"10%",
         marginBottom:"5%",
         borderRadius:"1px"
-    },LoginLayoutCheckBox:{
+    },LoginLayoutBoxText:{
+        marginLeft:"5%"
+    },
+
+    LoginLayoutCheckBox:{
         marginLeft:"10%",
         marginRight:"10%",
         marginBottom:"5%",
-        background: theme.palette.text.secondary
-    },LoginLayoutBoxText:{
-        marginLeft:"5%"
+    },icon: {
+        borderRadius: 3,
+        width: 16,
+        height: 16,
+        boxShadow: 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+        backgroundColor: '#f5f8fa',
+        backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+        '$root.Mui-focusVisible &': {
+            outline: '2px auto rgba(19,124,189,.6)',
+            outlineOffset: 2,
+        },
+        'input:hover ~ &': {
+            backgroundColor: '#ebf1f5',
+        },
+        'input:disabled ~ &': {
+            boxShadow: 'none',
+            background: 'rgba(206,217,224,.5)',
+        },
+    }, checkedIcon: {
+        backgroundColor: '#137cbd',
+        backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+        '&:before': {
+            display: 'block',
+            width: 16,
+            height: 16,
+            backgroundImage:
+                "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+                " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+                "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
+            content: '""',
+        },
+        'input:hover ~ &': {
+            backgroundColor: '#106ba3',
+        },
     },
 
     LoginLayoutButton:{
@@ -99,6 +136,43 @@ function SignUp(props){
     );
 }
 
+function StyledCheckbox(props) {
+    const classes = useStyles();
+
+    return (
+        <Checkbox
+            className={classes.root}
+            disableRipple
+            color="default"
+            checkedIcon={<span className={clsx(classes.icon, classes.checkedIcon)} />}
+            icon={<span className={classes.icon} />}
+            inputProps={{ 'aria-label': 'decorative checkbox' }}
+            {...props}
+        />
+    );
+}
+function RememberMeCheckBox(props){
+    const {classes} = props;
+
+    const [checked, setChecked] = React.useState(true);
+
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
+    return(
+        <FormControl className={classes.LoginLayoutCheckBox} >
+            <FormControlLabel control={
+                <StyledCheckbox
+                    checked={checked}
+                    onChange={handleChange}/>
+                }
+                label={"Remember me"}
+                />
+        </FormControl>
+    );
+}
+
 
 function LoginLayout(props){
     const {classes} = props;
@@ -106,17 +180,11 @@ function LoginLayout(props){
     const [values, setValues] = React.useState({
         id: '',
         password: '',
-        checked: false,
         showPassword: false,
     });
 
     const handleChange = (prop) => (event) => {
         setValues({ ...values, [prop]: event.target.value });
-    };
-
-
-    const handleCheckBoxChange = () =>{
-        setValues({ ...values, checked: !values.checked });
     };
 
     const handleClickShowPassword = () => {
@@ -188,14 +256,7 @@ function LoginLayout(props){
                            }
             />
 
-            <FormControl>
-                <FormControlLabel className={classes.LoginLayoutCheckBox}
-                                  control={<CheckBox />}
-                                  label={"Remember me"}
-                                  checked={values.checked}
-                                  onChange={handleCheckBoxChange}
-                />
-            </FormControl>
+            <RememberMeCheckBox classes={classes}/>
 
             <Button className={classes.LoginLayoutButton}
                 href="/signup"
