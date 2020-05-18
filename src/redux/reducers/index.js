d589b95e4d885d58bd85537f9615e3d146066368
BoscoTin <tintin111198@gmail.com>
2020-05-19 00:48:23 +0800

    redux demo for add del tag

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 /* Import all reducers from different files */
 import APIReducer from "../../API/Reducer"
 import TestReducer from "./TestReducer"
+import DemoTagsReducer from "./SelfTagDemo"
 
 const rootReducer = combineReducers({
     API: APIReducer,
-    test: TestReducer
+    test: TestReducer,
+    self: DemoTagsReducer
 })
 
 export default rootReducer
--- src/redux/store/InitialState.js
+++ src/redux/store/InitialState.js
@@ -1,5 +1,10 @@
 const initial = {
-
+  self: {
+    avatar: "avatar/avatar_1.jpeg",
+    displayname: "Sammy Lawson",
+    from: "Creative Director, Amazon",
+    tags: ["Marketing", "Finance", "Management Strategy"],
+  },
 };
 
-export default initial
+export default initial;
