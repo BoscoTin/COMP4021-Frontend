1e8a06cd88d7749ea1f285b1320af557a0946bc5
BoscoTin <tintin111198@gmail.com>
2020-05-19 12:35:25 +0800

    Find user by email redux

--- src/redux/actions/User.js
+++ src/redux/actions/User.js
@@ -7,13 +7,35 @@ import {
   BEGIN_SIGNUP,
   AUTH_SIGNUP_FAIL,
   SIGNUP_SUCCESS,
-  SIGNUP_FAIL
+  SIGNUP_FAIL,
+  BEGIN_FIND_USER,
+  FIND_USER_SUCCESS,
+  FIND_USER_FAIL,
 } from "../types/User";
 
+export function begin_find_user() {
+  return { type: BEGIN_FIND_USER };
+}
+
+export function find_user(email) {
+  return runAPI(
+    `/user/findByEmail?email=${email}`,
+    "GET",
+    success_find_user,
+    fail_find_user
+  );
+}
+
+function success_find_user(payload) {
+  return { type: FIND_USER_SUCCESS, payload };
+}
+
+function fail_find_user(payload) {
+  return { type: FIND_USER_FAIL, payload };
+}
+
 export function begin_login() {
-  return {
-    type: BEGIN_LOGIN,
-  };
+  return { type: BEGIN_LOGIN };
 }
 
 export function load_login(email, password) {
@@ -25,17 +47,11 @@ export function load_login(email, password) {
 }
 
 function success_login(payload) {
-  return {
-    type: LOGIN_SUCCESS,
-    payload,
-  };
+  return { type: LOGIN_SUCCESS, payload };
 }
 
 function fail_login(payload) {
-  return {
-    type: LOGIN_FAIL,
-    payload,
-  };
+  return { type: LOGIN_FAIL, payload };
 }
 
 export function begin_signup() {
@@ -43,25 +59,32 @@ export function begin_signup() {
 }
 
 export function load_signup(states) {
-  return runAPI("/auth/signup", "POST", success_auth_signup(states), fail_auth_signup, {
-    groupid: 777,
-    ...states
-  });
+  return runAPI(
+    "/auth/signup",
+    "POST",
+    success_auth_signup(states),
+    fail_auth_signup,
+    {
+      groupid: 777,
+      ...states,
+    }
+  );
 }
 
 function success_auth_signup(states) {
-  return (payload) => runAPI("/user/create", "POST", success_signup, fail_signup, {
-    groupid: 777,
-    ...states
-  })
+  return (payload) =>
+    runAPI("/user/create", "POST", success_signup, fail_signup, {
+      groupid: 777,
+      ...states,
+    });
 }
 
-function success_signup(payload){
-  return { type: SIGNUP_SUCCESS, payload }
+function success_signup(payload) {
+  return { type: SIGNUP_SUCCESS, payload };
 }
 
-function fail_auth_signup(payload){
-  return { type: AUTH_SIGNUP_FAIL, payload }
+function fail_auth_signup(payload) {
+  return { type: AUTH_SIGNUP_FAIL, payload };
 }
 
 function fail_signup(payload) {
--- src/redux/reducers/User.js
+++ src/redux/reducers/User.js
@@ -7,20 +7,26 @@ import {
   BEGIN_SIGNUP,
   SIGNUP_SUCCESS,
   SIGNUP_FAIL,
-  AUTH_SIGNUP_FAIL
+  AUTH_SIGNUP_FAIL,
+  BEGIN_FIND_USER,
+  FIND_USER_FAIL,
+  FIND_USER_SUCCESS
 } from "../types/User";
 
 const status = (state = "", action) => {
   switch (action.type) {
     case BEGIN_LOGIN:
     case BEGIN_SIGNUP:
+    case BEGIN_FIND_USER:
       return "loading";
     case LOGIN_SUCCESS:
     case SIGNUP_SUCCESS:
+    case FIND_USER_FAIL:
       return "success";
     case LOGIN_FAIL:
     case AUTH_SIGNUP_FAIL:
     case SIGNUP_FAIL:
+    case FIND_USER_SUCCESS:
       return "fail";
     default:
       return state;
@@ -45,10 +51,20 @@ const email = (state = "", action) => {
   }
 }
 
+const details = (state = {}, action) => {
+  switch (action.type) {
+    case FIND_USER_SUCCESS:
+      return action.payload.result
+    default:
+      return state
+  }
+}
+
 const reducer = combineReducers({
   status,
   message,
-  email
+  email,
+  details
 });
 
 export default reducer;
